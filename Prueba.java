18ab769e0597d2a1493151899db66d857472f7cc
Joel garsen <joel.garsen.1@educa.jcyl.es>
2023-10-04 13:51:52 +0200

    Grupo musica

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Garcia Sen");
 		System.out.println("19 16/03/2004");
 		System.out.println("Fabada");
+		System.out.println("Sabaton");
 	}
 
 }
